b1c8625b5e40bb7efe679ebdc04854b2cab67a3a
MariamGharibyan <marigharibyann@gmail.com>
2022-05-12 22:20:35 +0400

    This is Homework2 - FigurePainter2

--- src/homework/homework2/FigurePainter2.java
+++ src/homework/homework2/FigurePainter2.java
@@ -1,14 +1,13 @@
 package homework.homework2;
 
 public class FigurePainter2 {
-
-        public static void main(String[] args) {
-            /* 1.
-             * * * *
-             * * *
+    public static void main(String[] args) {
+        /* 1.
+         * * * *
+           * * *
              * *
-             *
-             */
+               *
+         */
 
 
 //        for (int i = 0; i < 4; i++) {
@@ -37,32 +36,33 @@ public class FigurePainter2 {
 
       */
 
-            for (int i = 0; i < 4; i++) {
-                for (int j = 4 - i; j >= 0; j--) {
-                    System.out.print(" ");
-                }
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("* ");
-
-                }
-                System.out.println();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4 - i; j >= 0; j--) {
+                System.out.print(" ");
             }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
 
-
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j <= i + 2; j++) {
-                    System.out.print(" ");
-
-                }
-                for (int j = 3 - i; j > 0; j--) {
-                    System.out.print("* ");
-                }
-
-                System.out.println();
             }
-
-
+            System.out.println();
         }
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i + 2; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 3 - i; j > 0; j--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+
     }
+}
+
 
 
