24d9e267c22c14bfbb2aeb2d73ea0ef75778ff75
MariamGharibyan <marigharibyann@gmail.com>
2022-05-12 20:56:32 +0400

    This is Homework2 - FigurePainter2

---
+++ src/homework/homework2/FigurePainter2.java
@@ -0,0 +1,66 @@
+package homework.homework2;
+
+public class FigurePainter2 {
+
+    public static void main(String[] args) {
+        /* 1.
+         * * * *
+         * * *
+         * *
+         *
+         */
+
+
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 0; j < 4 - i; j++) {
+//                System.out.print(" *");
+//            }
+//
+//            System.out.println();
+//        }
+
+
+//        ----------------------------------------------------------------------
+
+     /*
+      2.
+               *
+              * *
+             * * *
+            * * * *
+             * * *
+              * *
+               *
+
+      */
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4 - i; j >= 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i + 2; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 3 - i; j > 0; j--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
