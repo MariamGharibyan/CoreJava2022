80f959ac04634480d9fe80590fcc744ae2921e9d
MariamGharibyan <marigharibyann@gmail.com>
2022-05-12 22:16:19 +0400

    This is Homework2 - FigurePainter2

---
+++ src/classwork/lesson4/IfExample.java
@@ -0,0 +1,12 @@
+package classwork.lesson4;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if (x < y) {
+            System.out.println("x - ը  փոքր է y - ից");
+        }
+        System.out.println("after if");
+    }
+}
