ceca44e35f642edd13e9619e43f10141defe9f3a
MariamGharibyan <marigharibyann@gmail.com>
2022-05-12 19:11:47 +0400

    FigurePainter homework1

---
+++ src/homework/homework1/FigurePainter.java
@@ -0,0 +1,20 @@
+package homework.homework1;
+
+public class FigurePainter {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
