9a66a608b02460a81b8453d83aac9e4671868572
MariamGharibyan <105442275+MariamGharibyan@users.noreply.github.com>
2022-05-23 10:34:21 +0400

    This is Homework5 - ArraySort

--- src/homework/arrayutil/ArraySort.java
+++ src/homework/arrayutil/ArraySort.java
@@ -2,27 +2,9 @@ package homework.arrayUtil;
 
 public class ArraySort {
     public static void main(String[] args) {
-        int[] numbers = {1500, 400, 2000, 80, 1446, 16, 0, -7, -9, 180, 12, 120};
+        int[] numbers = {0, 800, 20, 200, 6, 16, 2, -70, -9, 10, 2, 7};
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length - 1; j++) {
-                if (numbers[j] < numbers[j + 1]) {
-                    int min = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = min;
-                }
-            }
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length - 1; j++) {
-                if (numbers[j] < numbers[j + 1]) {
-                    int min = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = min;
-                }
-            }
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length - 1; j++) {
+            for (int j = 0; j < numbers.length - 1; j++) {
                 if (numbers[j] < numbers[j + 1]) {
                     int min = numbers[j];
                     numbers[j] = numbers[j + 1];
@@ -35,27 +17,9 @@ public class ArraySort {
             System.out.print(numbers[i] + " ");
         }
 //        --------------------------------------------------------------------------------------
-        int[] numbersClone = {1500, -7, 400, 2000, 80, 1446, 16, -9, 0, 180, 12, 120};
+        int[] numbersClone = {1500, -7, 406, 200, 8000, 146, 16, -9, 0, 180, 12, 120};
         for (int i = 0; i < numbersClone.length; i++) {
-            for (int j = i; j < numbersClone.length - 1; j++) {
-                if (numbersClone[j] > numbersClone[j + 1]) {
-                    int max = numbersClone[j];
-                    numbersClone[j] = numbersClone[j + 1];
-                    numbersClone[j + 1] = max;
-                }
-            }
-        }
-        for (int i = 0; i < numbersClone.length; i++) {
-            for (int j = i; j < numbersClone.length - 1; j++) {
-                if (numbersClone[j] > numbersClone[j + 1]) {
-                    int max = numbersClone[j];
-                    numbersClone[j] = numbersClone[j + 1];
-                    numbersClone[j + 1] = max;
-                }
-            }
-        }
-        for (int i = 0; i < numbersClone.length; i++) {
-            for (int j = i; j < numbersClone.length - 1; j++) {
+            for (int j = 0; j < numbersClone.length - 1; j++) {
                 if (numbersClone[j] > numbersClone[j + 1]) {
                     int max = numbersClone[j];
                     numbersClone[j] = numbersClone[j + 1];
