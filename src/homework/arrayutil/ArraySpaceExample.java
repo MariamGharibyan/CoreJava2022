a2f32ab4a6c50689aa1e246821ed7426af37f383
MariamGharibyan <105442275+MariamGharibyan@users.noreply.github.com>
2022-05-24 21:48:49 +0400

    This is changed Homework5 - ArraySpaceExample

--- src/homework/arrayutil/ArraySpaceExample.java
+++ src/homework/arrayutil/ArraySpaceExample.java
@@ -2,22 +2,32 @@ package homework.arrayUtil;
 
 public class ArraySpaceExample {
     public static void main(String[] args) {
-        char[] spaceArray = {' ', ' ', ' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' ', ' '};
+        char[] spaceArray = {' ', ' ', ' ', ' ', 'c', 'a', 't', ' ', ' ', 'b', 'i', 'o', 'b', ' ', ' ', ' '};
+        int resultLengthFirst = 0;
+        int resultLengthLast = 0;
         int resultLength = 0;
         for (int i = 0; i < spaceArray.length; i++) {
-            if (spaceArray[i] != ' ') {
-                resultLength++;
+            if (spaceArray[i] == ' ') {
+                resultLengthFirst++;
+            } else {
+                break;
             }
         }
+        for (int i = spaceArray.length - 1; i < spaceArray.length; i--) {
+            if (spaceArray[i] == ' ') {
+                resultLengthLast++;
+            } else {
+                break;
+            }
+        }
+        resultLength = spaceArray.length - (resultLengthFirst + resultLengthLast);
         char[] result = new char[resultLength];
         int j = 0;
-        for (int i = 0; i < spaceArray.length; i++) {
-            if (spaceArray[i] != ' ') {
-                for (; j < result.length; ) {
-                    result[j] = spaceArray[i];
-                    j++;
-                    break;
-                }
+        for (int i = resultLengthFirst; i < spaceArray.length; i++) {
+            for (; j < result.length; ) {
+                result[j] = spaceArray[i];
+                j++;
+                break;
             }
         }
         for (int i = 0; i < result.length; i++) {
